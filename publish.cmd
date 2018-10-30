5092dbe64d6c711b75433fef6a56915f9c80e494
Vyacheslav Demydenko <vyde@ciklum.com>
2018-10-30 18:05:37 +0200

    re

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=kharkivbot-Web-Deploy.pubxml -p:Password=5MsxFx6Wwe6ad3JMsljodwnePfsicl7Coxs0DqfBBLL0gKmjCwmTt2aF71c6
+
